acd557c272f9e5e7c7d6ceb52e9a4d10d0ee40f2
chengxinjing <530501797@qq.com>
2019-09-05 00:42:17 +0800

    fix some issue

--- aws_web/scripts/startAwsService.sh
+++ aws_web/scripts/startAwsService.sh
@@ -3,4 +3,4 @@ timestamp=`date +'%Y%m%d%H%M%S'`
 credentialPath=/web/config/credentials.properties
 JAVAOption="-Xms1024m -Xmx1024m -Daws.credential=${credentialPath}"
 jar=`ls /web | grep 'aws_web'`
-java ${JAVAOption} -jar /app/${jar} > /web/log/aws_web.${timestamp}.log
+java ${JAVAOption} -jar /web/${jar} > /web/log/aws_web.${timestamp}.log
