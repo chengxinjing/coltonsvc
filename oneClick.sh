2e040a549b6d7851e94b58f453b638485497f3ab
chengxinjing <530501797@qq.com>
2019-09-16 21:50:27 +0800

    modify the oneClick

--- oneClick.sh
+++ oneClick.sh
@@ -68,8 +68,8 @@ echo "secretKey=" >> $absoulteDir/web/config/credentials.properties
 hostname=`hostname -I | awk '{print $1}'`
 url="http://${hostname}:8080/aws/download/"
 
-docker run -d  -v $absoulteDir/gemfire/log:/gemfire/log -v $absoulteDir/gemfire/bin:/gemfire/bin --name gemfire_${version} -p 31431:31431 -p 8092:8092  -P -e LOCATORS=localhost[31431] aws_gemfire:v1.0.${version}
+docker run -d  -v $absoulteDir/gemfire/log:/gemfire/log  --name gemfire_${version} -p 31431:31431 -p 8092:8092  -P -e LOCATORS=localhost[31431] aws_gemfire:v1.0.${version}
 
 docker run -d -v $absoulteDir/batch/log:/batch/log -v $absoulteDir/batch/feed:/batch/feed -v $absoulteDir/batch/bin:/batch/bin --name batch_${version} -p 9092:9092 -e LOCATOR_HOST=gemfire -e LOCATOR_PORT=31431 -e DOWNLOAD_URL=${url}  --link gemfire_${version}:gemfire aws_batch:v1.0.${version}
 
-docker run -d -v $absoulteDir/web/log:/web/log -v $absoulteDir/web/bin:/web/bin -v $absoulteDir/web/config:/web/config --name web_${version} -p 8080:8080 aws_web:v1.0.${version}
+docker run -d -v $absoulteDir/web/log:/web/log  -v $absoulteDir/web/config:/web/config --name web_${version} -p 8080:8080 aws_web:v1.0.${version}
